fe9c8c13f19ca9fa8bf3f54632b326a54dd83f22
nisdar <kristjan.radsin@gmail.com>
2025-05-22 14:28:02 +0300

    algne main menu

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@
 using namespace std;
 using namespace ftxui;
 
+//https://github.com/ArthurSonzogni/FTXUI?tab=readme-ov-file
+
 Element make_grid2(int ruudustik[SUURUS][SUURUS]) {
     std::vector<Elements> rows;
     for (int i = 0; i < SUURUS; ++i) {
@@ -31,12 +33,13 @@ Element make_grid2(int ruudustik[SUURUS][SUURUS]) {
 
 int main() {
     auto screen = ScreenInteractive::TerminalOutput();
-
     int ruudustik[SUURUS][SUURUS];
+
     looRuudustik(ruudustik);
 
-    auto count = 0;
-    bool liigutas = true;
+    int gamestate = 0;
+
+    bool liigutas = false;
     bool gameover = false;
 
     auto gridRenderer = Renderer([&] {
@@ -45,42 +48,95 @@ int main() {
             gameoverText = "Mäng läbi!";
         }
         return vbox(make_grid2(ruudustik), text(gameoverText));  //
-  });
+    });
 
-    auto component = Container::Horizontal({gridRenderer});
-    component |= CatchEvent([&](Event event) {
-        liigutas = false;
-        bool handled = false;
-        if (event == Event::ArrowUp) {
-            liigutaRuudustikku(ruudustik, 0, &liigutas);
-            handled = true;
-        }
-        if (event == Event::ArrowDown) {
-            liigutaRuudustikku(ruudustik, 1, &liigutas);
-            handled = true;
-        }
-        if (event == Event::ArrowLeft) {
-            liigutaRuudustikku(ruudustik, 2, &liigutas);
-            handled = true;
-        }
-        if (event == Event::ArrowRight) {
-            liigutaRuudustikku(ruudustik, 3, &liigutas);
-            handled = true;
-        }
-        if (liigutas) {
-            lisaNumber(ruudustik);
-            if (kasLabi(ruudustik)) {
-                gameover = true;
-            }
-        }
-        if (event == Event::Escape) {
+    vector<string> valikud = {"Mängi", "Seaded", "Sulge"};
+    int selected = 0;
+    MenuOption option = MenuOption::Vertical();
+    option.on_enter = [&]{
+        if (selected == 2) {
             screen.Exit();
         }
+        if (selected == 1) {
+            liigutas = false;
+            gameover = false;
+            looRuudustik(ruudustik);
+        }
+        gamestate = selected+1;
+    };
+    auto mainMenu = Menu(&valikud,
+    &selected, option);
 
+    //TODO
+    auto seaded = Container::Horizontal({
+        Renderer([&] {
+            return text("TODO");
+        })
+    });
+
+    int state = 0;
+    auto tab_container = Container::Tab(
+        {
+            mainMenu,
+            gridRenderer,
+            seaded,
+        },
+        &gamestate
+    );
+
+    auto main_component = Container::Vertical({tab_container});
+    main_component |= CatchEvent([&](Event event) {
+        bool handled = false;
+        if (gamestate == 1) {
+            liigutas = false;
+            if (event == Event::ArrowUp) {
+                liigutaRuudustikku(ruudustik, 0, &liigutas);
+                handled = true;
+            }
+            if (event == Event::ArrowDown) {
+               liigutaRuudustikku(ruudustik, 1, &liigutas);
+                handled = true;
+            }
+            if (event == Event::ArrowLeft) {
+                liigutaRuudustikku(ruudustik, 2, &liigutas);
+                handled = true;
+            }
+            if (event == Event::ArrowRight) {
+                liigutaRuudustikku(ruudustik, 3, &liigutas);
+                handled = true;
+            }
+            if (liigutas) {
+               lisaNumber(ruudustik);
+               if (kasLabi(ruudustik)) {
+                    gameover = true;
+                }
+            }
+            if (event == Event::Escape) {
+                gamestate = 0;
+            }
+        }
+        if (gamestate == 2) {
+            if (event == Event::Escape) {
+                gamestate = 0;
+            }
+        }
         return handled;
     });
 
-    screen.Loop(component);
+    auto main_renderer = Renderer(main_component, [&] {
+        return vbox({
+            hbox(text("selected = "), text(std::to_string(selected))),
+            hbox(text("gamestate = "), text(std::to_string(gamestate))),
+            hbox(text("ESC viib tagasi main menusse")),
+            separator(),
+            main_component->Render() | frame,
+        }) |
+            border;
+    });
+
+    cout << "\e[8;40;100t"; //Muudab algse terminai suurust
+
+    screen.Loop(main_renderer);
 
     return 0;
 }
